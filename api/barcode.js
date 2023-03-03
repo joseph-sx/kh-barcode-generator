23cd5b0bc02be9f2abaeb1fb0bcbde8f77556a0b
Joseph García <joseph@krakenhub.net>
2023-03-02 18:16:55 -0600

    Barcode api done, TODO: complete format required and security

---
+++ api/barcode.js
@@ -0,0 +1,23 @@
+const { DOMImplementation, XMLSerializer } = require('xmldom');
+var JsBarcode = require('jsbarcode');
+
+module.exports = (req, res) => {
+
+  const xmlSerializer = new XMLSerializer();
+  const document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+  const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+
+
+  res.setHeader('content-type', 'image/svg+xml');
+
+  var value = req.query.v;
+  JsBarcode(svgNode, value, {
+    xmlDocument: document,
+    width:1,
+    fontSize: 15,
+    height: 25,
+  });
+  const svgText = xmlSerializer.serializeToString(svgNode);
+  res.send(svgText);
+};
